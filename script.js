ab2624c86b39f53884932f9203e9e73c2f2950a6
ferrante <brandon.f70@gmail.com>
2022-06-23 16:11:09 +0200

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function check(){
+    var nbr;
+    nbr = Number(document.getElementById("year").value);
+    if(nbr >2004)
+    {
+       alert("Vous n'êtes pas un adulte");
+    }
+    else
+    {
+       alert("Vous êtes un adulte");
+    }
+
+   
+}
